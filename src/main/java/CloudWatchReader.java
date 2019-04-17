df7fe479c0a3211a8cbbcc22d5005b468f73acd1
Dan Torrey <dan@graylog.com>
2019-04-17 17:48:03 -0500

    Add ability to write logs for testing purposes

--- src/main/java/CloudWatchReader.java
+++ src/main/java/CloudWatchReader.java
@@ -1,37 +1,90 @@
+import org.joda.time.DateTime;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient;
 import software.amazon.awssdk.services.cloudwatchlogs.model.DescribeLogStreamsRequest;
 import software.amazon.awssdk.services.cloudwatchlogs.model.GetLogEventsRequest;
+import software.amazon.awssdk.services.cloudwatchlogs.model.InputLogEvent;
+import software.amazon.awssdk.services.cloudwatchlogs.model.PutLogEventsRequest;
 
+import java.util.Arrays;
+
+/**
+ * A simple AWS CloudWatch client that writes a log entry, then reads all logs in the group back.
+ *
+ * This code requires that the following environment variables are specified:
+ *
+ * AWS_ACCESS_KEY_ID
+ * AWS_SECRET_ACCESS_KEY
+ * AWS_REGION (eg. us-east-1)
+ * AWS_LOG_GROUP (eg. test-logs)
+ */
 public class CloudWatchReader {
 
     public static final String AWS_REGION_ENVIRONMENT_VARIABLE = "AWS_REGION";
     public static final String LOG_GROUP_NAME = "AWS_LOG_GROUP";
 
     /**
-     * Simple sample client that reads logs from an AWS log group.
+     * Write a log, then read logs back.
      */
     public static void main(String[] args) {
+        writeLogs();
+        getLogs();
+    }
 
+    /**
+     * Write a log message into CloudWatch.
+     */
+    private static void writeLogs() {
+        CloudWatchLogsClient cwe =
+                CloudWatchLogsClient.builder().build();
+
+        String logMessageText = "{ \"key1\": \"value11\", \"key2\": \"value2\" }";
+        InputLogEvent logEvent = InputLogEvent.builder().message(logMessageText).timestamp(DateTime.now().getMillis()).build();
+
+        CloudWatchLogsClient logsClient = CloudWatchLogsClient.builder()
+                                                              .region(Region.of(System.getenv(AWS_REGION_ENVIRONMENT_VARIABLE)))
+                                                              .build();
+
+        // Get the first log stream name. We'll pull logs from this for now.
+        DescribeLogStreamsRequest logStreamsRequest = DescribeLogStreamsRequest.builder()
+                                                                               .logGroupName(getLogGroupName())
+                                                                               .build();
+
+        String sequenceToken = logsClient.describeLogStreams(logStreamsRequest).logStreams().get(0).uploadSequenceToken();
+
+        PutLogEventsRequest request = PutLogEventsRequest.builder()
+                                                         .logEvents(Arrays.asList(logEvent))
+                                                         .logGroupName(getLogGroupName())
+                                                         .logStreamName(getLogGroupName())
+                                                         // Sequence token is required so that the log can be written to the latest location.
+                                                         .sequenceToken(sequenceToken)
+                                                         .build();
+        cwe.putLogEvents(request);
+    }
+
+    /**
+     * Query the log messages from CloudWatch.
+     */
+    private static void getLogs() {
         // Credentials are explicitly provided by environment variables.
         // See https://docs.aws.amazon.com/sdk-for-java/v2/developer-guide/credentials.html
         CloudWatchLogsClient logsClient = CloudWatchLogsClient.builder()
                                                               .region(Region.of(System.getenv(AWS_REGION_ENVIRONMENT_VARIABLE)))
                                                               .build();
         DescribeLogStreamsRequest build = DescribeLogStreamsRequest.builder()
-                .logGroupName("integrations-flowlogs")
-                .build();
+                                                                   .logGroupName(getLogGroupName())
+                                                                   .build();
 
         // Get the first log stream name. We'll pull logs from this for now.
         String firstLogStreamName = logsClient.describeLogStreams(build).logStreams().get(0).logStreamName();
 
         // Get a page of log events from the first stream in the log group and print it to the console.
+
         logsClient.getLogEvents(GetLogEventsRequest.builder()
                                                    .logStreamName(firstLogStreamName)
                                                    .logGroupName(getLogGroupName())
                                                    .build())
-                  .events().stream().forEach(System.out::println);
-
+                  .events().forEach(System.out::println);
     }
 
     /**
